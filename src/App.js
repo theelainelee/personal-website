6d4914d8a1291000a82b1f12339dc45925de5fc4
Elaine Lee <elaine.lee@elainel-mbp.ff0000.com>
2018-04-16 17:04:35 -0700

    add name title to gh-pages

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Elaine Lee | Developer | Programmer </h1>
+          <h1 className="App-title">Elaine Lee </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Creative Developer and Programmer
         </p>
       </div>
     );
