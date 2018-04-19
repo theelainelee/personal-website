42a532a3a0a4ff3a30f3151e89034c39da70b059
Elaine Lee <elaine.lee@elainel-mbp.ff0000.com>
2018-04-18 17:01:43 -0700

    added video player and logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import ReactPlayer from 'react-player';
+
+const VIDEOSTYLE = {
+  backgroundColor: 'white'
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Elaine Lee </h1>
-        </header>
-        <p className="App-intro">
-          Creative Developer and Programmer
-        </p>
+      <div className="Parent">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-name">Elaine Lee </h1>
+
+             <p className="App-title">
+            Creative Developer and Programmer
+            </p>
+          </header>
+          <div>      </div>
+          <p className="App-contact">
+            CONTACT ME
+          </p>
+        </div>
+        <div className="App-video" >
+          <ReactPlayer 
+            url='https://vimeo.com/243556536'
+            className='react-player'
+            playing
+            controls
+            width='100%'
+            height='300px'
+            style={VIDEOSTYLE}
+
+
+          />
+        </div>
       </div>
     );
   }
 }
+
+
 
 export default App;
