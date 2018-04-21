8a335f06c0d1acd2037c1bb8d1d7b5c45ab70c7a
Elaine Lee <elaine.lee@elainel-mbp.ff0000.com>
2018-04-20 22:59:29 -0700

    add hamburger menu javascript code

--- src/App.js
+++ src/App.js
@@ -5,40 +5,57 @@ import ReactPlayer from 'react-player';
 // import ReactDOM from 'react-dom';
 // import Media from 'react-media';
 
-const VIDEOSTYLE = {
-  backgroundColor: 'white'
-};
+// const VIDEOSTYLE = {
+//   backgroundColor: 'white'
+// };
 
 
 
 class App extends Component {
 
-  burgerToggle() {
-    let linksEl = document.querySelector('.narrowLinks');
-    if (linksEl.style.display === 'block') {
-      linksEl.style.display = 'none';
-    } else {
-      linksEl.style.display = 'block';
-    }
+  hamburgerToggle() {
+    let hamburger = document.querySelector('.hamburger-menu');
+
+      
+        hamburger.addEventListener("click", function(){
+          console.log('CLICKED')
+          // this.classList.toggle("is-active");
+          if (this.classList.toggle('is-active')){
+            console.log("OPEN MENU SHOW X")
+            function x_openMenu(){
+              document.getElementById('hamburger--3dxy.is-active');
+            }
+          } else {
+            console.log("CLOSE MENU SHOW 3 LINES")
+            function threeLines_closeMenu(){
+              document.getElementById('hamburger--3dxy');
+            }
+          }
+        });
+      
   };
 
   render() {
     return (
       <div className="Parent">
-       <div className="hamburgerMenu" ></div>
+       <button type="button" className="hamburger-menu hamburger--3dxy" onClick={this.hamburgerToggle}>
 
+          <span className="hamburger-box" >
+            <span className="hamburger-inner" ></span>
+          </span>
+       </button>
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-name">Elaine Lee </h1>
 
              <p className="App-title">
-            Creative Developer and Programmer
+            Developer and Designer
             </p>
           </header>
           <div>      </div>
           <p className="App-contact">
-            CONTACT ME
+            Start a Project
           </p>
         </div>
         <div className="App-video" >
@@ -48,7 +65,7 @@ class App extends Component {
             controls
             width='100%'
             height='300px'
-            style={VIDEOSTYLE}
+            // style={VIDEOSTYLE}
 
 
           />
