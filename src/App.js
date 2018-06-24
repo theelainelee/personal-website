b639ed55bf6d2c99e5288643f4f00cdf5958f0fe
Elaine Lee <elaneylee@gmail.com>
2018-06-23 19:40:02 -0700

    add particles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './el_logo.png';
 import './App.css';
 import ReactPlayer from 'react-player';
+import Particles from 'react-particles-js'
 // import ReactDOM from 'react-dom';
 // import Media from 'react-media';
 
@@ -44,13 +45,127 @@ class App extends Component {
              </nav>
        
         <div className="App">
+       
           <header className="App-header">
+            <Particles className="particles-js" 
+             params={{
+                  "particles": {
+                  "number": {
+                    "value": 100,
+                    "density": {
+                      "enable": true,
+                      "value_area": 600
+                    }
+                  },
+                  "color": {
+                    "value": "#ffffff"
+                  },
+                  "shape": {
+                    "type": "circle",
+                    "stroke": {
+                      "width": 0,
+                      "color": "#000000"
+                    },
+                    "polygon": {
+                      "nb_sides": 5
+                    },
+                    "image": {
+                      "src": "img/github.svg",
+                      "width": 100,
+                      "height": 100
+                    }
+                  },
+                  "opacity": {
+                    "value": 0.5,
+                    "random": false,
+                    "anim": {
+                      "enable": true,
+                      "speed": 2,
+                      "opacity_min": 0.1,
+                      "sync": true
+                    }
+                  },
+                  "size": {
+                    "value": 3,
+                    "random": true,
+                    "anim": {
+                      "enable": false,
+                      "speed": 30,
+                      "size_min": 0.1,
+                      "sync": false
+                    }
+                  },
+                  "line_linked": {
+                    "enable": true,
+                    "distance": 150,
+                    "color": "#ffffff",
+                    "opacity": 0.5,
+                    "width": 1
+                  },
+                  "move": {
+                    "enable": true,
+                    "speed": 6,
+                    "direction": "none",
+                    "random": false,
+                    "straight": false,
+                    "out_mode": "out",
+                    "bounce": false,
+                    "attract": {
+                      "enable": false,
+                      "rotateX": 600,
+                      "rotateY": 1200
+                    }
+                  }
+                },
+                "interactivity": {
+                  "detect_on": "canvas",
+                  "events": {
+                    "onhover": {
+                      "enable": true,
+                      "mode": "grab"
+                    },
+                    "onclick": {
+                      "enable": true,
+                      "mode": "push"
+                    },
+                    "resize": true
+                  },
+                  "modes": {
+                    "grab": {
+                      "distance": 200,
+                      "line_linked": {
+                        "opacity": 1
+                      }
+                    },
+                    "bubble": {
+                      "distance": 200,
+                      "size": 40,
+                      "duration": 2,
+                      "opacity": 8,
+                      "speed": 3
+                    },
+                    "repulse": {
+                      "distance": 200,
+                      "duration": 0.4
+                    },
+                    "push": {
+                      "particles_nb": 4
+                    },
+                    "remove": {
+                      "particles_nb": 2
+                    }
+                  }
+                },
+                "retina_detect": true
+              }}
+            />
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-name">Elaine Lee </h1>
 
              <p className="App-title">
             Developer. Engineer.
             </p>
+            
           </header>
           <div>      </div>
           <button className="App-contact" onClick={this.animatePage}>
