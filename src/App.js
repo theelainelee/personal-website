dcec38fed8deb6c4610d466cd38a6311b65eaf7c
Elaine Lee <elaine.lee@ff0000.com>
2019-04-23 17:03:41 -0700

    add routes bg color

--- src/App.js
+++ src/App.js
@@ -35,6 +35,18 @@ class App extends Component {
     document.documentElement.style.overflow = 'scroll';
     document.body.scroll = "yes";
   }
+   closeOverlayAbout = () =>{
+
+    console.log('HIDE OVERLAY')
+    let openMenu = document.querySelector(".nav-menu");
+    openMenu.style.display = "none";
+    window.scrollTo({
+      top: window.innerHeight - 200,
+      behavior: 'smooth'
+    });
+    document.documentElement.style.overflow = 'scroll';
+    document.body.scroll = "yes";
+  }
    
     
 
@@ -54,7 +66,7 @@ class App extends Component {
        <nav className="nav-menu" >
                <li onClick={this.closeOverlay}>home</li>
                <li onClick={this.closeOverlay}>portfolio</li>
-               <li onClick={this.closeOverlay}>about</li>
+               <li onClick={this.closeOverlayAbout}>about</li>
                <li onClick={this.closeOverlay}>contact</li>
              </nav>
        
@@ -140,32 +152,32 @@ class App extends Component {
       </div>
       <div id='about-container'>
       <h1>ABOUT</h1>
-       <h3 className='left-about-text'>Hello! My name is Elaine and welcome to my world!  
-       I got my start in programming during my senior year at Cornell University.  
-       I had finished all of my required courses and decided to take a computer programming course as an elective.  
-       I took CS101 Intro to Computer Science. I learned about COBOL, C++, C# and the evolution of programming.  
-       It was a great foundation for my future career as a consummate programmer. My first job out of college was at Computer Sciences Corporation as a computer analyst.  
-       I had the best computer programming training with this company.  
-       While I left the industry to try my hand at Wall Street, I eventually found my way back to my first love and passion.  
-       I also found myself moving from the East Coast to the West. 
-       To get up-to-date quickly on the modern technology stack in my new city of Los Angeles, I invested in obtaining a technical foundation.  
-       I took an immersive full-time web development course at General Assembly.  
-       During that time Ruby on Rails and AngularJS were quite popular and hence I learned those languages.
-       After graduation I immediately received a job offer at Beachbody LLC in Santa Monica. 
-       Learning on the job is something you cannot pay to obtain. 
-       </h3>
-       <h3 className='right-about-text'>
-       During my 2 1/2 years at Beachbody I created landing pages, work on exciting new features, and worked in a high-performing team. 
-       I learned the difference between merging and rebasing, used Sass, and worked with a CMS.  
-       I wanted to expand my programming skills by exploring Javascript animations and creating more unique, innovative web applications. 
-       I began my career in the ad tech space at RED Interactive Agency, part of the EGM (Endeavour) family.  
-       I have been at RED now for 2 years and created a multitude of web applications for our clients ranging from Nike, Hulu, Netflix, Disney, and Paramount.  
-       I have also created responsive email templates and websites.  
-       As always in my desire to keep up-to-date with modern technology, I have been using ReactJS for the past 2 years building both production sites as well as personal projects.  
-       I was also able to explore using a lower K weight version of React, called Preact.  The best way to continue growing and to solidify knowledge is to teach.  
-       I was a part-time evening teaching assistant at UCLA for their programming course. 
-       This is me in a nutshell. <br/> Pleasure to have you visit my world! 
-       </h3>
+         <div className='left-about-text'>Hello! My name is Elaine and welcome to my world!  
+         I got my start in programming during my senior year at Cornell University.  
+         I had finished all of my required courses and decided to take a computer programming course as an elective.  
+         I took CS101 Intro to Computer Science. I learned about COBOL, C++, C# and the evolution of programming.  
+         It was a great foundation for my future career as a consummate programmer. My first job out of college was at Computer Sciences Corporation as a computer analyst.  
+         I had the best computer programming training with this company.  
+         While I left the industry to try my hand at Wall Street, I eventually found my way back to my first love and passion.  
+         I also found myself moving from the East Coast to the West. 
+         To get up-to-date quickly on the modern technology stack in my new city of Los Angeles, I invested in obtaining a technical foundation.  
+         I took an immersive full-time web development course at General Assembly.  
+         During that time Ruby on Rails and AngularJS were quite popular and hence I learned those languages.
+         After graduation I immediately received a job offer at Beachbody LLC in Santa Monica. 
+         Learning on the job is something you cannot pay to obtain. 
+         </div>
+         <div className='right-about-text'>
+         During my 2 1/2 years at Beachbody I created landing pages, work on exciting new features, and worked in a high-performing team. 
+         I learned the difference between merging and rebasing, used Sass, and worked with a CMS.  
+         I wanted to expand my programming skills by exploring Javascript animations and creating more unique, innovative web applications. 
+         I began my career in the ad tech space at RED Interactive Agency, part of the EGM (Endeavour) family.  
+         I have been at RED now for 2 years and created a multitude of web applications for our clients ranging from Nike, Hulu, Netflix, Disney, and Paramount.  
+         I have also created responsive email templates and websites.  
+         As always in my desire to keep up-to-date with modern technology, I have been using ReactJS for the past 2 years building both production sites as well as personal projects.  
+         I was also able to explore using a lower K weight version of React, called Preact.  The best way to continue growing and to solidify knowledge is to teach.  
+         I was a part-time evening teaching assistant at UCLA for their programming course. 
+         This is me in a nutshell. <br/> Pleasure to have you visit my world! 
+         </div>
        </div>
      </div>
       </ParallaxProvider>
